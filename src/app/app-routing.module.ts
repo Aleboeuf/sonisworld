48f30bcd6846ac928f9709f958fed872e30463ab
Alexis Leboeuf <alexis.leboeuf77@gmail.com>
2022-09-05 22:01:37 +0200

    1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './components/pages/home-page/home-page.component';
+import { TestPageComponent } from './components/pages/test-page/test-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'homepage', component: HomePageComponent },
+  { path: 'testpage', component: TestPageComponent },
+  { path: '', redirectTo: 'homepage', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TestPageComponent } from './components/pages/test-page/test-page.component';
+import { NavbarComponent } from './components/utils/navbar/navbar.component';
+import { HomePageComponent } from './components/pages/home-page/home-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestPageComponent,
+    NavbarComponent,
+    HomePageComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
